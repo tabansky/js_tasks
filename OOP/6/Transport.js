a423c9c4272541a204b64836be5b0743f88b84fe
TabanschiAlex <talex.alexandr@gmail.com>
2021-02-06 23:19:48 +0200

    обновлена 6 задача, но не выполнена

--- OOP/6/Transport.js
+++ OOP/6/Transport.js
@@ -1,5 +1,26 @@
 class Transport {
+    currentYear = new Date().getFullYear();
+    model;
+    price;
+    year;
 
+    constructor(model, price, year) {
+        this.model = model;
+        this.price = price;
+        this.year = year;
+    }
+
+    getModel() {
+        return this.model;
+    }
+
+    getAge() {
+        return this.currentYear - this.year;
+    }
+
+    getMovement() {
+        return 'хз что!'
+    }
 }
 
 module.exports = Transport;
