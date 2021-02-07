cffa613c7856520c70c9aa366b886084c008c46e
TabanschiAlex <talex.alexandr@gmail.com>
2021-02-07 23:52:00 +0200

    доработаны некоторые задачи

--- DOM/17/main.js
+++ DOM/17/main.js
@@ -2,7 +2,10 @@ const elem = document.getElementById('elem');
 
 document.addEventListener('DOMContentLoaded', () => {
     elem.addEventListener('click', (e) => {
-        console.log(elem.style.width = (parseInt(e.target.style.width) * 2).toString() + 'px')
-        console.log(elem.style.height = (parseInt(e.target.style.height) * 2).toString() + 'px')
+        elem.style.width = (parseInt(e.target.style.width) * 2).toString() + 'px';
+        elem.style.height = (parseInt(e.target.style.height) * 2).toString() + 'px';
+
+        console.log(elem.style.width);
+        console.log(elem.style.height);
     })
 });
--- DOM/6/main.js
+++ DOM/6/main.js
@@ -3,21 +3,21 @@ const numbers = ['1', '2', '3', '4', '5', '6', '7', '8'];
 const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
 const generateTable = () => {
-    let count = false;
 
     letters.forEach((letter, index) => {
         tableBody.innerHTML += `<tr id="${letter}"></tr>`;
-        count = !count;
 
         numbers.forEach((number, i) => {
-            if (count) {
+            if ((index % 2) && !(i % 2)) {
                 document.getElementById(letter).innerHTML +=
                     `<td id="${letter + number}" style="background: brown">${letter + number}</td>`;
-                count = false;
+
+            } else if (!(index % 2) && (i % 2)){
+                document.getElementById(letter).innerHTML +=
+                    `<td id="${letter + number}" style="background: brown">${letter + number}</td>`;
             } else {
                 document.getElementById(letter).innerHTML +=
                     `<td id="${letter + number}">${letter + number}</td>`;
-                count = true;
             }
 
         });
