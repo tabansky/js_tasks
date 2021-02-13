9a117b52d0f59e797304181bec4b3fcb1bf1e5c8
TabanschiAlex <talex.alexandr@gmail.com>
2021-02-13 17:59:55 +0200

    доработана TIME/2 задача

--- TIME/2/main.js
+++ TIME/2/main.js
@@ -2,20 +2,22 @@ const clock = document.getElementById('clock');
 const stopBtn = document.getElementById('stop');
 const startBtn = document.getElementById('start');
 
-const getCurrentTime = () => {
+const showCurrentTime = () => {
     const date = new Date();
 
     clock.innerHTML =
         `<span class="red">${date.getHours()}</span>` + ' : ' +
         `<span class="green">${date.getMinutes()}</span>` + ' : ' +
         `<span class="blue">${date.getSeconds()}</span>`;
+}
 
-    startBtn.addEventListener('click', getCurrentTime);
-    stopBtn.addEventListener('click', () => clearTimeout(recursion));
+const events = () => {
+    let recursion = setInterval(showCurrentTime, 1000);
 
-    const recursion = setTimeout(getCurrentTime, 1000);
+    startBtn.addEventListener('click', () => recursion = setInterval(showCurrentTime, 1000));
+    stopBtn.addEventListener('click', () => clearInterval(recursion));
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    getCurrentTime();
+    events();
 });
