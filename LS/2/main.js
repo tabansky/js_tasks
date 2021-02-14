870cb072dc3c7fbe9e067a69703e00e50110dde7
TabanschiAlex <talex.alexandr@gmail.com>
2021-02-14 15:18:03 +0200

    выполнены все задачи

---
+++ LS/2/main.js
@@ -0,0 +1,63 @@
+const todosTable = document.getElementById('todos');
+const input = document.getElementById('input');
+const sendBtn = document.getElementById('btn');
+const todoList = {};
+
+const showTodos = (id) => {
+    const tr = document.createElement('tr');
+    const tdValue = document.createElement('td');
+    const tdClose = document.createElement('td');
+
+    tdValue.innerText = id;
+    tdClose.setAttribute('class', 'close');
+    tdClose.setAttribute('id', id);
+    tdClose.innerHTML = `&#10006`;
+    tr.append(tdValue);
+    tr.append(tdClose);
+
+    todoList[tdValue.innerText] = tdValue.innerText;
+    todosTable.appendChild(tr);
+}
+
+const loadFromLocalStorage = () => {
+    for (const todo of Object.keys(localStorage)) {
+        showTodos(localStorage.getItem(todo));
+    }
+}
+
+const saveInLocalStorage = () => {
+    localStorage.clear();
+
+    for (const todo in todoList) {
+        localStorage.setItem(todo, todo);
+    }
+}
+
+const addToList = () => {
+    sendBtn.addEventListener('click', () => {
+        if (input.value) {
+            showTodos(input.value);
+            saveInLocalStorage();
+            input.value = '';
+        }
+
+        listenToDel();
+    });
+};
+
+const listenToDel = () => {
+    const deleteButtons = document.querySelectorAll('.close');
+
+    deleteButtons.forEach((btn, index) => {
+        deleteButtons[index].addEventListener('click', () => {
+            btn.parentElement.remove();
+            delete todoList[`${btn.id}`];
+            saveInLocalStorage();
+        })
+    });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadFromLocalStorage();
+    addToList();
+});
